949312a2dd375ab90f3b66cb10401349c037013e
unknown <karkeeniksan@gmail.com>
2021-12-09 15:10:48 +0900

    redux (product fetch and handle cart)

---
+++ frontend/src/components/Message.js
@@ -0,0 +1,8 @@
+import { Alert } from "react-bootstrap";
+export const Message = ({ variant, children }) => {
+  return <Alert variant={variant}>{children}</Alert>;
+};
+
+Message.defaultProps = {
+  variant: "info",
+};
